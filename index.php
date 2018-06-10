12e013e3479b6ef91bff0533b53a229c11d0a7d5
Maximiliano Quevedo <maxquevedo@gmail.com>
2018-06-10 01:39:04 -0400

    dejando bonito el index

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
 	require('head.php');
 ?>
-<div class="row-lg-12">
-	<div class="col-lg-4 offset-sm-9">
+<div class="row" style="margin-top: 2%; margin-bottom: 2%;">
+	<div class="col-lg-7">
+		<article>
+			<p>El Hospital de Pelotillehue, con ustedes desde el año 1990 brindando atencion de primera calidad y a muy buen precio amplía su alcance saltando a las nuevas tecnologías, nos modernizamos construyendo un hermoso y funcional sitio para todos nuestros clientes.</p>
+			<p>Rapidez y una atención de calidad son nuestro sello profesional, tenemos los mejores profesionales esperando a atenderte.</p>
+			<p>Nos ubicamos entre Buenas Peras y Cumpeo</p>
+			<p>¡Atiendete con nosotros hoy! </p>
+		</article>
+	</div>
+	<div class="col-lg-5">
 		<img class="img-fluid" width="100%" src="img/ambulancia.jpg">
 	</div>
 </div>
-
+<div class="row">
+	
+</div>
 <?php
 	require('footer.php');
 ?>
