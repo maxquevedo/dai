57e231fdacc42400a538daacc0a68d4edbfd8979
chcastillog <38799268+chcastillog@users.noreply.github.com>
2018-06-10 23:24:17 -0400

    ya no quiero seguir tocando nada

--- iniciarSesion.php
+++ iniciarSesion.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="form-signin" role="form">
             <h3 class="form-signin-heading col-lg-9 font-weight-bold offset-md-2 text-center">Ingrese sus datos</h3>
-            <input type="email" id="usuario" class="form-control" placeholder="Usuario" required autofocus>
+            <input type="text" id="usuario" class="form-control" placeholder="Usuario" required autofocus>
             <input type="password" id="pass" class="form-control" placeholder="Contraseña" required>
             <button class="btn btn-lg btn-primary btn-block" id="singin" type="button">ingresar</button>
         </div>
